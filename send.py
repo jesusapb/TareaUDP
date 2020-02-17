80093b59ee96b80d538463319dcafb3b39e1f586
Unknown <jesusapb29@gmail.com>
2020-02-16 23:13:22 -0600

    version uno
    versión con comentarios y leyendas en español

--- send.py
+++ send.py
@@ -3,23 +3,26 @@
 
 import socket
 from sys import argv
+#To create a socket, you must use the socket.socket() function available in socket module, which has
+#the general syntax:
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-target = input("Send to: ")
-file = input("File to send: ")
+target = input("Enviar a: ")
+file = input("Direccion del archivo: ")
 #main program
 try:
         #Open file
         try:
-                print("Reading file...", end=" ")
+                print("Leyendo archivo ...", end=" ")
                 data = open(file, "rb").read()
-                print("done")
+                print("Hecho")
         except FileNotFoundError:
-                print("No such File")
+                print("Error de lectura")
                 s.close()
                 exit()
         #send file
-        print("Sending file...", end=" ")
+        print("Enviando archivo ...", end=" ")
+        # "s.sendto()" This method transmits UDP message
         s.sendto(data, (target, 50002))
-        print("done")
+        print("Hecho")
 finally:
         s.close()
